758658d68a05f3086fec12739284976a9d487a0c
chen <chen@123.com>
2021-08-02 17:33:57 +0800

    first commit

---
+++ src/main/java/com/bingge/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.bingge.git;
+
+/**
+ * @author chenjianbing
+ * @create 2021-08-02 17:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
