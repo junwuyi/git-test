59d6b825ab30ac32dc21960ec192ecc93c918a4f
chenjb <chenjb@123.com>
2021-08-03 17:22:13 +0800

    second commit

--- src/main/java/com/bingge/git/GitTest.java
+++ src/main/java/com/bingge/git/GitTest.java
@@ -2,10 +2,10 @@ package com.bingge.git;
 
 /**
  * @author chenjianbing
- * @create 2021-08-02 17:28
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!2");
     }
 }
